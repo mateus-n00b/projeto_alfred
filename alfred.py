54e8a974641d64d181d95b61d25e8ea6631277e6
mateus <mateus.aluufc@gmail.com>
2016-12-20 00:42:53 -0300

    Add files via upload

---
+++ alfred.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Simples script para execucao de comandos de voz
+# O codigo foi baseado no exemplo presente em
+# https://pythonspot.com/en/speech-recognition-using-google-speech-api/
+#
+# Mateus-n00b, Dezembro 2016
+#
+# Versao 1.0
+#
+# Licensa GPL
+# -================================================================-
+
+import speech_recognition as sr
+import os
+import commands
+
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("What do you need sir?")
+    audio = r.listen(source)
+
+# Speech recognition using Google Speech Recognition
+try:
+    # Get the commands from the commands.py
+    cmds = commands.Commands()
+    cmds = cmds.getCommand()
+    for x in cmds.keys():
+        if x in str(r.recognize_google(audio)).lower():
+            os.system(cmds[x])
+            break
+        else:
+            print "You said "+r.recognize_google(audio)
+except sr.UnknownValueError:
+    print("What do you said?")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
